69b7b1fde77d95c0dee62ed6699b8075e815f8c1
Cousin 1 <be.morre@gmail.com>
2024-09-10 12:52:41 -0400

    Fix button

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+const clickMe = document.getElementById("clickMe");
+clickMe.addEventListener("click", helloLog);
 
-function helloLog() {
-    console.log('Hello World');
-  };
+function helloLog(e) {
+  console.log("Helrld", e);
+}
