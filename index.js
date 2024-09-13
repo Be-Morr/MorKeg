1d812e303ac24c99a17f44be4b05029178be2d11
Cousin 1 <be.morre@gmail.com>
2024-09-12 23:16:31 -0400

    Testing

--- index.js
+++ index.js
@@ -2,5 +2,9 @@ const clickMe = document.getElementById("clickMe");
 clickMe.addEventListener("click", helloLog);
 
 function helloLog(e) {
-  console.log("Helrld", e);
+  console.log("Hello World", e);
+}
+
+for (let hashVar = 0; hashVar <= 7; hashVar += 1) {
+  console.log(hashVar);
 }
