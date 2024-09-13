866910afcbd96edd90e19c12f244c0a433455cab
Be-Morr <165185403+Be-Morr@users.noreply.github.com>
2024-09-13 17:32:33 -0400

    Th12 (#2)
    * Testing
    
    * JS HW
    
    * HW addition

--- index.js
+++ index.js
@@ -2,5 +2,21 @@ const clickMe = document.getElementById("clickMe");
 clickMe.addEventListener("click", helloLog);
 
 function helloLog(e) {
-  console.log("Helrld", e);
+  console.log("Hello World", e);
+}
+
+for (let hashVar = "#"; hashVar.length <= 7; hashVar += "#") {
+  console.log(hashVar);
+}
+
+for (let numb = 1; numb <= 100; numb += 1) {
+  if (numb % 3 === 0 || numb % 5 === 0) {
+    console.log("FizzBuzz");
+  } else if (numb % 3 === 0) {
+    console.log("Fizz");
+  } else if (numb % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(numb);
+  }
 }
