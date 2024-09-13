1916c982027875379029686307e2365076b73c43
Cousin 1 <be.morre@gmail.com>
2024-09-13 17:21:05 -0400

    JS HW

--- index.js
+++ index.js
@@ -5,6 +5,18 @@ function helloLog(e) {
   console.log("Hello World", e);
 }
 
-for (let hashVar = 0; hashVar <= 7; hashVar += 1) {
+for (let hashVar = "#"; hashVar.length <= 7; hashVar += "#") {
   console.log(hashVar);
+}
+
+for (let numb = 1; numb <= 100; numb += 1) {
+  if (numb % 3 === 0 || numb % 5 === 0) {
+    console.log("FizzBuzz");
+  } else if (numb % 3 === 0) {
+    console.log("Fizz");
+  } else if (numb % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(numb);
+  }
 }
