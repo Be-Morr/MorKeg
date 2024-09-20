7d08281254e638299d70745870aae2169ff0a029
Cousin 1 <be.morre@gmail.com>
2024-09-20 14:22:36 -0400

    sep20kata

--- dailyKata/sept18.js
+++ dailyKata/sept18.js
@@ -11,7 +11,7 @@
 // Output = "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11"
 
 function alphabetPosition(text) {
-  const stringAlphabet = "abcdefghijklmnopqrstuvwyxz";
+  const stringAlphabet = "abcdefghijklmnopqrstuvwxyz";
   let numericalLetter = "";
   let lowercaseText = text.toLowerCase();
   for (
