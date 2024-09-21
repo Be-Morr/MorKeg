3310e12d017c41201271d9cc0d863587bf97b768
Cousin 1 <be.morre@gmail.com>
2024-09-21 14:53:39 -0400

    Fixed?

--- index.js
+++ index.js
@@ -5,21 +5,21 @@ function helloLog(e) {
   console.log("Hello World", e);
 }
 
-for (let hashVar = "#"; hashVar.length <= 7; hashVar += "#") {
-  console.log(hashVar);
-}
+// for (let hashVar = "#"; hashVar.length <= 7; hashVar += "#") {
+//   console.log(hashVar);
+// }
 
-for (let numb = 1; numb <= 100; numb += 1) {
-  if (numb % 3 === 0 || numb % 5 === 0) {
-    console.log("FizzBuzz");
-  } else if (numb % 3 === 0) {
-    console.log("Fizz");
-  } else if (numb % 5 === 0) {
-    console.log("Buzz");
-  } else {
-    console.log(numb);
-  }
-}
+// for (let numb = 1; numb <= 100; numb += 1) {
+//   if (numb % 3 === 0 || numb % 5 === 0) {
+//     console.log("FizzBuzz");
+//   } else if (numb % 3 === 0) {
+//     console.log("Fizz");
+//   } else if (numb % 5 === 0) {
+//     console.log("Buzz");
+//   } else {
+//     console.log(numb);
+//   }
+// }
 // You can get the Nth character, or letter, from a string by writing [N] after the string (for example, string[2]). The resulting value will be a string containing only one character (for example, "b"). The first character has position 0, which causes the last one to be found at position string.length - 1. In other words, a two-character string has length 2, and its characters have positions 0 and 1.
 
 // Write a function called countBs that takes a string as its only argument and returns a number that indicates how many uppercase B characters there are in the string.
@@ -28,18 +28,18 @@ for (let numb = 1; numb <= 100; numb += 1) {
 
 // Your code here.
 
-→ 2
-console.log(countChar("kakkerlak", "k"));
-→ 4
+// → 2
+// console.log(countChar("kakkerlak", "k"));
+// → 4
 
-const countBs = (word) => {
-  let numberOfB = 0;
-  for (let i = 0; i < word.length; i++) {
-    const char = word.charAt(i);
-    if (char === "B") {
-      numberOfB++;
-    }
-  }
-  return numberOfB;
-};
-console.log(countBs("BBbbOB"));
+// const countBs = (word) => {
+//   let numberOfB = 0;
+//   for (let i = 0; i < word.length; i++) {
+//     const char = word.charAt(i);
+//     if (char === "B") {
+//       numberOfB++;
+//     }
+//   }
+//   return numberOfB;
+// };
+// console.log(countBs("BBbbOB"));
