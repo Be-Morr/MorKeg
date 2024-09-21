8e89a5b6729dc713aa1475d72194a729fc3f9253
Cousin 1 <be.morre@gmail.com>
2024-09-21 14:34:42 -0400

    sep 21 kata

--- dailyKata/sep20.js
+++ dailyKata/sep20.js
@@ -16,12 +16,20 @@ const basicOp = (operation, value1, value2) => {
     return value1 - value2;
   } else if (operation === "*") {
     return value1 * value2;
-  } else if (operation === "/"){
-    return value1 / value2
+  } else if (operation === "/") {
+    return value1 / value2;
   }
 };
 //  const basicOp = (operation, value1, value2) => {
-//     return eval(value1 + operation + value2);   
+//     return eval(value1 + operation + value2);
 //  }
 // failed because initially used , instead of + inside of eval()
- 
+
+// Timmy & Sarah think they are in love, but around where they live, they will only know once they pick a flower each. If one of the flowers has an even number of petals and the other has an odd number of petals it means they are in love.
+
+// Write a function that will take the number of petals of each flower and return true if they are in love and false if they aren't.
+
+function lovefunc(flower1, flower2) {
+  return flower1 % 2 !== flower2 % 2;
+  // moment of truth
+}
