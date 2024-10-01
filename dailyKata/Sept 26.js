0a77bfb73be5b36aa478521a5ec4dc457ca8445f
Cousin 1 <be.morre@gmail.com>
2024-10-01 09:24:15 -0400

    Kata upload

--- dailyKata/Sept 26.js
+++ dailyKata/Sept 26.js
@@ -49,4 +49,94 @@ function removeChar(str){
     
     
     
-// Format as =>  const removeChar = str => str.slice(1,-1)    
+// Format as =>  const removeChar = str => str.slice(1,-1)
+
+
+// If you can't sleep, just count sheep!!
+// Task:
+
+// Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
+
+
+var countSheep = function (num){
+  let snoring = '';
+
+  for (i = 1; i <= num; i++){
+    const sheep = i + " sheep...";
+   snoring += sheep;
+  }
+  return snoring;
+}
+
+// Much cleaner solution using "${}"
+// const countSheep = num => {
+//   let snoring = "";
+//   for (let i=1; i <= num; i++){
+//     str+= '${i} sheep...';}
+//   return snoring;
+// }
+
+
+// Introduction
+
+// The first century spans from the year 1 up to and including the year 100, the second century - from the year 101 up to and including the year 200, etc.
+// Task
+
+// Given a year, return the century it is in.
+// Examples
+
+// 1705 --> 18
+// 1900 --> 19
+// 1601 --> 17
+// 2000 --> 20
+// 2742 --> 28
+
+// Note: this kata uses strict construction as shown in the description and the examples, you can read more about it here
+
+const century = year => {
+  const centuryDecimal = (year / 100);
+  const centuryName = (Math.ceil(centuryDecimal));
+  return centuryName;
+}
+
+// Math can be used inside of Math.cei() i.e.
+
+// const century = year => Math.ceil(year/100)
+
+
+
+// Your task is to create a function that does four basic mathematical operations.
+
+// The function should take three arguments - operation(string/char), value1(number), value2(number).
+// The function should return result of numbers after applying the chosen operation.
+// Examples(Operator, value1, value2) --> output
+
+// ('+', 4, 7) --> 11
+// ('-', 15, 18) --> -3
+// ('*', 5, 5) --> 25
+// ('/', 49, 7) --> 7
+
+
+
+const basicOp = (operation, value1, value2) => {
+  if (operation === "+") {
+    return value1 + value2;
+  } else if (operation === "-") {
+    return value1 - value2;
+  } else if (operation === "*") {
+    return value1 * value2;
+  } else if (operation === "/"){
+    return value1 / value2
+  }
+};
+
+// Can also be done as a switch :
+// function basicOp(operation, value1, value2) {
+//   switch (operation) {
+//       case '+': return value1 + value2;
+//       case '-': return value1 - value2;
+//       case '*': return value1 * value2;
+//       case '/': return value1 / value2;
+//       default: return 'Operation must be one of + - * /';
+//   }
+// }
