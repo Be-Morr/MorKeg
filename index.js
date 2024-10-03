9938faed737be27800c6079ec833f6be8fb6edd5
Be-Morr <165185403+Be-Morr@users.noreply.github.com>
2024-10-03 14:57:45 -0400

    New branch (#7)
    * Testing
    
    * JS HW
    
    * HW addition
    
    * 1.1
    
    * 1.2
    
    * 1.3
    
    * Sept 19 Kata
    
    * Fixed space on kata 9.17
    
    * sep20kata
    
    * sep 21 kata
    
    * Fixing changes
    
    * Fixed?
    
    * Katas
    
    * dailykata
    
    * Kata upload
    
    * Kata and Chapter 4
    
    * Array kata

--- index.js
+++ index.js
@@ -4,23 +4,6 @@ clickMe.addEventListener("click", helloLog);
 function helloLog(e) {
   console.log("Hello World", e);
 
-}
-// for (let hashVar = "#"; hashVar.length <= 7; hashVar += "#") {
-//   console.log(hashVar);
-// }
-
-// for (let numb = 1; numb <= 100; numb += 1) {
-//   if (numb % 3 === 0 || numb % 5 === 0) {
-//     console.log("FizzBuzz");
-//   } else if (numb % 3 === 0) {
-//     console.log("Fizz");
-//   } else if (numb % 5 === 0) {
-//     console.log("Buzz");
-//   } else {
-//     console.log(numb);
-//   }
-
-// }
 
 // for (let hashVar = "#"; hashVar.length <= 7; hashVar += "#") {
 //   console.log(hashVar);
@@ -58,13 +41,5 @@ function helloLog(e) {
   //   }
   // }
   // return numberOfB;
-//   let numberOfB = 0;
-//   for (let i = 0; i < word.length; i++) {
-//     const char = word.charAt(i);
-//     if (char === "B") {
-//       numberOfB++;
-//     }
-//   }
-//   return numberOfB;
 // };
 // console.log(countBs("BBbbOB"));
